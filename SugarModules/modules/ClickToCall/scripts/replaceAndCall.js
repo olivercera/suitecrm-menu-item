9da0d65a37905403dea98980d19068ca8a5258ea
Oliver Cera <olivercera@gmail.com>
2019-04-24 15:30:52 -0500

    initial test

---
+++ SugarModules/modules/ClickToCall/scripts/replaceAndCall.js
@@ -0,0 +1,28 @@
+
+var phones = $('[type=phone]');
+var baseUrl = window.siteUrl;
+
+for (i = 0; i < phones.length; i++) {
+
+    var phone = $(phones[i]);
+    var callerButton = $(phone).attr('caller-button');
+
+    if (callerButton === undefined) {
+
+        var number = phone.text().trim();
+
+        phone.append('<img src="' + baseUrl + '/themes/SuiteP/images/Calls.svg" alt="to call" class="click-to-call" data-number="' + number + '" />');
+        $(phone).attr('caller-button', true);
+
+    }
+
+}
+
+$('img.click-to-call:not(.binded)').addClass('binded').on('click', function() {
+
+    var number = $(this).attr('data-number');
+
+    alert('CCP: call to ' + number);
+
+});
+
