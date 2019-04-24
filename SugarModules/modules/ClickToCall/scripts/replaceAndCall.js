74bcd2a699124dd2800bdc6c8a17a6aecb529c5b
4nacs4 <4nacs4@gmailcom>
2019-04-24 18:20:05 -0400

    some tweaks to add ccp link button

--- SugarModules/modules/ClickToCall/scripts/replaceAndCall.js
+++ SugarModules/modules/ClickToCall/scripts/replaceAndCall.js
@@ -1,5 +1,6 @@
 
 var phones = $('[type=phone]');
+var navBase = $('#toolbar').find('.nav');
 var baseUrl = window.siteUrl;
 
 for (i = 0; i < phones.length; i++) {
@@ -17,12 +18,18 @@ for (i = 0; i < phones.length; i++) {
     }
 
 }
+/*if($('#ccpItem').length == 0){
+    navBase.append('<li id="ccpItem" class="topnav"><span class="notCurrentTabLeft">&nbsp;</span><span class="notCurrentTab"><a href="#" id="grouptab_5" class="dropdown-toggle grouptab">CCP</a><span class="notCurrentTabRight">&nbsp;</span></li>')
+
+    $('#ccpItem').on('click', function() {
+        alert('CCP: call to ' + number); 
+    });
+}*/
 
 $('img.click-to-call:not(.binded)').addClass('binded').on('click', function() {
-
     var number = $(this).attr('data-number');
-
-    alert('CCP: call to ' + number);
+    //alert('CCP: call to ' + number); 
+    window.open('https://vf-demo.awsapps.com/connect/ccp', '', 'width=340px,height=630px');
 
 });
 
